4da9a6700db0eff2d3c2d7fb1168b50cf31a0569
Alaina <alainaajce93@gmail.com>
2025-10-28 09:17:09 +0530

    updated

--- C/recursive.c
+++ C/recursive.c
@@ -1,69 +1,103 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
 char input[100];
 int i = 0;
-int error = 0;
 
-void E();
-void Eprime();
-void T();
-void Tprime();
-void F();
+// Function declarations
+int E();
+int EP();
+int T();
+int TP();
+int F();
 
 int main() {
-    printf("Enter input string: ");
-    gets(input);  // use with caution (lab version)
-    E();
+    printf("\nRecursive Descent Parsing for the grammar:\n");
+    printf("E  -> T E'\n");
+    printf("E' -> + T E' | ε\n");
+    printf("T  -> F T'\n");
+    printf("T' -> * F T' | ε\n");
+    printf("F  -> (E) | id\n");
 
-    if (strlen(input) == i && error == 0) {
-        printf("Input accepted.\n");
-    } else {
-        printf("Error!\nInput rejected.\n");
-    }
+    printf("\nEnter the string to be checked: ");
+    scanf("%s", input);
+
+    if (E() && input[i] == '\0')
+        printf("\nString is accepted\n");
+    else
+        printf("\nString is not accepted\n");
 
     return 0;
 }
 
-void E() {
-    T();
-    Eprime();
+// E → T E'
+int E() {
+    if (T()) {
+        if (EP())
+            return 1;
+        else
+            return 0;
+    } else
+        return 0;
 }
 
-void Eprime() {
+// E' → + T E' | ε
+int EP() {
     if (input[i] == '+') {
         i++;
-        T();
-        Eprime();
+        if (T()) {
+            if (EP())
+                return 1;
+            else
+                return 0;
+        } else
+            return 0;
     }
+    return 1; // epsilon
 }
 
-void T() {
-    F();
-    Tprime();
+// T → F T'
+int T() {
+    if (F()) {
+        if (TP())
+            return 1;
+        else
+            return 0;
+    } else
+        return 0;
 }
 
-void Tprime() {
+// T' → * F T' | ε
+int TP() {
     if (input[i] == '*') {
         i++;
-        F();
-        Tprime();
+        if (F()) {
+            if (TP())
+                return 1;
+            else
+                return 0;
+        } else
+            return 0;
     }
+    return 1; // epsilon
 }
 
-void F() {
-    if (isalpha(input[i])) {
+// F → (E) | id
+int F() {
+    if (input[i] == '(') {
         i++;
-    } else if (input[i] == '(') {
+        if (E()) {
+            if (input[i] == ')') {
+                i++;
+                return 1;
+            } else
+                return 0;
+        } else
+            return 0;
+    } else if (isalpha(input[i])) { // id
         i++;
-        E();
-        if (input[i] == ')') {
-            i++;
-        } else {
-            error = 1;
-        }
-    } else {
-        error = 1;
-    }
+        return 1;
+    } else
+        return 0;
 }
